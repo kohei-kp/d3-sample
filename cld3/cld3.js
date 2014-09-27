3bda4771c2b4740eca246c8c3f9d88db3825c419
kohei-kp <cowhey1991@gmail.com>
2014-09-27 17:13:16 +0900

    見た目修正など。データをとりあえずtest data

--- cld3/cld3.js
+++ cld3/cld3.js
@@ -1,154 +1,260 @@
-var db = new WF_LOG.DatabaseAccess(),
+var //db = new WF_LOG.DatabaseAccess(),
     //upsize = db.getCountUpSize(),
-    login = db.getHourLogin(),
-    datas = [],
+    //login = db.getHourLogin(),
+    //datas = [],
     map, projection;
 
-login.done(function (data) {
-  var h = 500, w = 960,
-      padding = 80,
+createPunchCard();
+createJapanMap();
+// Weekly Login Chart
+function createPunchCard() {
+  var width = 800, height = 400, padding = 80;
 
-      // days
-      days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday','Saturday'],
-
-      // hours
+  // days and hours
+  var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
       hours = [
-        '12am', '1am', '2am', '3am', '4am', '5am', '6am', '7am', 
-        '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm',
-        '4pm', '5pm', '6pm', '7pm', '8pm', '9pm', '10pm', '11pm'
-      ],
+        '12a', '1a', '2a', '3a', '4a', '5a', '6a', '7a',
+        '8a', '9a', '10a', '11a', '12p', '1p', '2p', '3p',
+        '4p', '5p', '6p', '7p', '8p', '9p', '10p', '11p'
+      ];
 
-      // palette
-      palette = d3.select('#login').append('svg').attr({ width: w, height: h}),
+  // dataset example
+  var dataset = [
+    // sunday
+    {'DAY_COUNT': '1', 'COUNT': '22', 'HOUR': '2'},
+    {'DAY_COUNT': '1', 'COUNT': '2', 'HOUR': '4'},
+    {'DAY_COUNT': '1', 'COUNT': '66', 'HOUR': '8'},
+    {'DAY_COUNT': '1', 'COUNT': '123', 'HOUR': '10'},
+    {'DAY_COUNT': '1', 'COUNT': '45', 'HOUR': '12'},
+    {'DAY_COUNT': '1', 'COUNT': '62', 'HOUR': '13'},
+    {'DAY_COUNT': '1', 'COUNT': '32', 'HOUR': '16'},
 
-      // groups
-      dayGroup = palette.append('g'),
-      hourGroup = palette.append('g'),
-      circleGroup = palette.append('g'),
+    // monday
+    {'DAY_COUNT': '2', 'COUNT': '11', 'HOUR': '0'},
+    {'DAY_COUNT': '2', 'COUNT': '1', 'HOUR': '8'},
+    {'DAY_COUNT': '2', 'COUNT': '88', 'HOUR': '9'},
+    {'DAY_COUNT': '2', 'COUNT': '100', 'HOUR': '11'},
+    {'DAY_COUNT': '2', 'COUNT': '33', 'HOUR': '15'},
+    {'DAY_COUNT': '2', 'COUNT': '67', 'HOUR': '16'},
+    {'DAY_COUNT': '2', 'COUNT': '77', 'HOUR': '17'},
 
-      x = {min: 0, max: w - padding, step: (w - padding) / 24},
+    // tuesday
+    {'DAY_COUNT': '3', 'COUNT': '21', 'HOUR': '3'},
+    {'DAY_COUNT': '3', 'COUNT': '4', 'HOUR': '5'},
+    {'DAY_COUNT': '3', 'COUNT': '7', 'HOUR': '7'},
+    {'DAY_COUNT': '3', 'COUNT': '66', 'HOUR': '13'},
+    {'DAY_COUNT': '3', 'COUNT': '22', 'HOUR': '14'},
+    {'DAY_COUNT': '3', 'COUNT': '11', 'HOUR': '15'},
+    {'DAY_COUNT': '3', 'COUNT': '99', 'HOUR': '22'},
 
-      y = {min: 0, max: h - 30, step: (h - 30) / 7},
+    // wednesday
+    {'DAY_COUNT': '4', 'COUNT': '2', 'HOUR': '4'},
+    {'DAY_COUNT': '4', 'COUNT': '2', 'HOUR': '6'},
+    {'DAY_COUNT': '4', 'COUNT': '1', 'HOUR': '10'},
+    {'DAY_COUNT': '4', 'COUNT': '12', 'HOUR': '12'},
+    {'DAY_COUNT': '4', 'COUNT': '12', 'HOUR': '16'},
+    {'DAY_COUNT': '4', 'COUNT': '20', 'HOUR': '17'},
+    {'DAY_COUNT': '4', 'COUNT': '11', 'HOUR': '23'},
 
-      // append days
-      dayText = dayGroup.selectAll('text').data(days).enter().append('text'),
-      dayLabels = dayText.attr({
-        'x': 0,
-        'y': function (d) {
-          return y.step * (days.indexOf(d) + 1);
-        }
-      })
-      .text(function (d) { return d; })
-      .attr({
-        'font-family': 'sams-serif',
-        'font-size': '12px'
-      }),
+    // thursday
+    {'DAY_COUNT': '5', 'COUNT': '22', 'HOUR': '8'},
+    {'DAY_COUNT': '5', 'COUNT': '30', 'HOUR': '9'},
+    {'DAY_COUNT': '5', 'COUNT': '40', 'HOUR': '10'},
+    {'DAY_COUNT': '5', 'COUNT': '15', 'HOUR': '12'},
+    {'DAY_COUNT': '5', 'COUNT': '12', 'HOUR': '14'},
+    {'DAY_COUNT': '5', 'COUNT': '50', 'HOUR': '17'},
+    {'DAY_COUNT': '5', 'COUNT': '32', 'HOUR': '20'},
 
-      // append hour
-      hourText = hourGroup.selectAll('text').data(hours).enter().append('text'),
-      hourLabels = hourText.attr({
-        'x': function (d) {
-          return x.step * (hours.indexOf(d) + 1) + 32;
-        },
-        'y': y.max + 20
-      })
-      .text(function (d) { return d; })
-      .attr({
-        'font-family': 'sans-serif',
-        'font-size': '12px'
-      }),
+    // friday
+    {'DAY_COUNT': '6', 'COUNT': '10', 'HOUR': '9'},
+    {'DAY_COUNT': '6', 'COUNT': '40', 'HOUR': '11'},
+    {'DAY_COUNT': '6', 'COUNT': '50', 'HOUR': '13'},
+    {'DAY_COUNT': '6', 'COUNT': '60', 'HOUR': '15'},
+    {'DAY_COUNT': '6', 'COUNT': '70', 'HOUR': '17'},
+    {'DAY_COUNT': '6', 'COUNT': '60', 'HOUR': '19'},
+    {'DAY_COUNT': '6', 'COUNT': '32', 'HOUR': '21'},
 
-      // scale
-      scaleData = [],
-      datas = [],
-      z,
-      c,
-      tuple, logins,
-      cy, cx, r, title;
+    // saturday
+    {'DAY_COUNT': '7', 'COUNT': '9', 'HOUR': '0'},
+    {'DAY_COUNT': '7', 'COUNT': '2', 'HOUR': '1'},
+    {'DAY_COUNT': '7', 'COUNT': '1', 'HOUR': '3'},
+    {'DAY_COUNT': '7', 'COUNT': '4', 'HOUR': '8'},
+    {'DAY_COUNT': '7', 'COUNT': '9', 'HOUR': '10'},
+    {'DAY_COUNT': '7', 'COUNT': '4', 'HOUR': '16'},
+    {'DAY_COUNT': '7', 'COUNT': '2', 'HOUR': '18'}
 
-    data.forEach(function (d, i) {
-      datas[i] = [parseInt(d.DAY_COUNT) ,parseInt(d.COUNT), parseInt(d.HOUR)];
-      scaleData[i] = parseInt(d.COUNT);
-    });
+  ];
 
-    z = {
-      data: scaleData
-    };
+  // svg
+  var svg = d3.select('#login').append('svg').attr({ width: width, height: height});
 
-    z.max = d3.max(z.data);
-    z.min = d3.min(z.data);
-    z.domain = [z.min, z.max];
-    z.range = [4, 15];
-    z.scale = d3.scale.linear().domain(z.domain).range(z.range);
+  // group
+  var day = svg.append('g'),
+      hour = svg.append('g'),
+      circle = svg.append('g');
 
-    for (var i in datas) {
-      tuple = datas[i];
-      logins = tuple[1];
+  var x = {min: 0, max: width - padding, step: (width - 80) / 24},
+      y = {min: 0, max: height - 30, step: (height - 30) / 7},
+      z;
 
-      if (logins > 0) {
-        cy = y.step * (tuple[0] + 1);
-        cx = x.step * (tuple[2] + 1) + 50;
-        r = z.scale(logins);
-        title = 'Login:' + logins;
+  var dayAxis, hourAxis;
 
-        c = circleGroup.append('circle')
-          .attr({
-            cx: cx,
-            cy: cy,
-            r: r,
-            id: logins,
-            title: title,
-            class: 'hover-circle'
-          })
-          .on('mouseover', function (d) {
-            d3.select(this).attr('fill', 'orange');
-
-            var xPos = parseFloat(d3.select(this).attr('cx')) + 20,
-                yPos = parseFloat(d3.select(this).attr('cy')) - 20;
-
-            palette.append('text')
-              .attr({
-                'id': 'tooltip',
-                'x': xPos,
-                'y': yPos,
-                'text-anchor': 'middle',
-                'font-family': 'sans-serif',
-                'font-size': '11px',
-                'font-weight': 'bold',
-                'fill': 'black'
-              })
-              .text(d3.select(this).attr('id'));
-          })
-          .on('mouseout', function () {
-            d3.select(this).attr('fill', 'black');
-            d3.select('#tooltip').remove();
-          });
-      }
+  // xAxis
+  dayAxis = day.selectAll('text').data(days).enter().append('text');
+  dayAxis.attr({
+    'x': 0,
+    'y': function (d) {
+      return y.step * (days.indexOf(d) + 1);
     }
-});
+  })
+  .text(function (d) { return d; })
+  .attr({
+    'font-family': 'sans-serif',
+    'font-size': '12px'
+  });
 
-d3.json('json/japan.topojson', function (d) {
-  var w = 650, h = 650, path,
-      features = topojson.feature(d, d.objects.japan).features;
+  // yAxis
+  hourTxt = hour.selectAll('text').data(hours).enter().append('text');
+  hourTxt.attr({
+    'x': function (d) {
+      return x.step * (hours.indexOf(d) + 1) + 40;
+    },
+    'y': y.max + 20
+  })
+  .text(function (d) { return d; })
+  .attr({
+    'font-family': 'sans-serif',
+    'font-size': '12px'
+  });
 
-  projection = d3.geo.mercator()
-    .center([139.5, 35.7])
-    .translate([w / 2, h /2])
-    .scale([1400]),
-  path = d3.geo.path().projection(projection),
 
-  map = d3.select('#map').append('svg').attr({ width: w, height: h }),
+  var datas = [], scaleData = [];
 
-  map.selectAll('path').data(features).enter()
-    .append('path')
-    .attr({
-      stroke: 'black',
-      'stroke-width': '0.5',
-      d: path,
-      fill: '#669966'
-    });
-});
+  dataset.forEach(function (d, i) {
+    datas[i] = [parseInt(d.DAY_COUNT), parseInt(d.COUNT), parseInt(d.HOUR)];
+    scaleData[i] = parseInt(d.COUNT);
+  });
 
+  var domain = [d3.min(scaleData), d3.max(scaleData)];
+
+  z = d3.scale.linear().domain(domain).range([4, 15]);
+
+  var i = 0, len = datas.length, cache, logins,
+      cx, cy, r, title;
+
+  for (i = 0; i < len; i += 1) {
+    cache = datas[i];
+    logins = cache[1];
+
+    if (logins > 0) {
+      cy = y.step * (cache[0]);
+      cx = x.step * (cache[2] + 1) + 50;
+      r = z(logins);
+      title = 'Login:' + logins;
+
+      circle.append('circle')
+        .attr({
+          cx: cx,
+          cy: cy,
+          r: r,
+          id: logins,
+          title: title,
+          class: 'hover-circle',
+          fill: '#3A3A3A'
+        })
+        .on('mouseover', function (d) {
+          d3.select(this).attr('fill', 'steelblue');
+
+          var xPos = parseFloat(d3.select(this).attr('cx')) - 40,
+              yPos = parseFloat(d3.select(this).attr('cy')) - 40;
+
+          svg.append('rect')
+            .attr({
+              id: 'tooltip-rect',
+              x: xPos, y: yPos, rx: 6, ry: 6,
+              width: 90, height: 25,
+              fill: '#444444', stroke: '#C3C3C3', 'stroke-width': 1
+            });
+
+          svg.append('text')
+            .attr({
+              id: 'tooltip',
+              x: xPos + 47,
+              y: yPos + 16,
+              'text-anchor': 'middle',
+              'font-family': 'sans-serif',
+              'font-size': '11px',
+              'font-weight': 'bold',
+              'fill': 'white'
+            })
+            .text(d3.select(this).attr('id') + ' Logins');
+        })
+        .on('mouseout', function () {
+          d3.select(this).attr('fill', '#3A3A3A');
+          d3.select('#tooltip').remove();
+          d3.select('#tooltip-rect').remove();
+        });
+    }
+  }
+}
+
+function createJapanMap() {
+  d3.json('../json/japan.topojson', function (d) {
+    var w = 650, h = 650, path, circle,
+        features = topojson.feature(d, d.objects.japan).features;
+
+    var dataset = [
+      { 'Prefecture': 'Kobe' , 'lon': '135.195', 'lat': '34.690' },
+      { 'Prefecture': 'Tokyo' , 'lon': '139.691', 'lat': '35.089' },
+      { 'Prefecture': 'Hokkaido' , 'lon': '141.346', 'lat': '43.064' },
+      { 'Prefecture': 'Fukuoka' , 'lon': '130.407', 'lat': '33.590' },
+      { 'Prefecture': 'Tokushima' , 'lon': '134.559', 'lat': '34.065' },
+      { 'Prefecture': 'Osaka' , 'lon': '135.502', 'lat': '34.693' },
+      { 'Prefecture': 'Nigata' , 'lon': '139.036', 'lat': '37.916' },
+      { 'Prefecture': 'Iwate' , 'lon': '139.036', 'lat': '37.916' }
+    ];
+
+    projection = d3.geo.mercator()
+      .center([139.5, 35.7])
+      .translate([w / 2, h /2])
+      .scale([1400]),
+    path = d3.geo.path().projection(projection),
+
+    map = d3.select('#map').append('svg').attr({ width: w, height: h }),
+
+    map.selectAll('path').data(features).enter()
+      .append('path')
+      .attr({
+        stroke: 'black',
+        'stroke-width': '0.5',
+        d: path,
+        fill: '#669966'
+      });
+
+    circle = map.selectAll('circle').data(dataset).enter()
+      .append('circle')
+      .attr({
+        cx: function (d) {
+          return projection([d.lon, d.lat])[0];
+        },
+        cy: function (d) {
+          return projection([d.lon, d.lat])[1];
+        },
+        r: 1,
+        fill: 'red',
+        opacity: 1
+      })
+      .transition()
+      .duration(700)
+      .attr({
+        r: 5,
+        opacity: 0.6
+      });
+  });
+};
+
+
+/*
 $(function () {
   // 地図へのマッピング
   $('#mapping-btn').click(function () {
@@ -330,10 +436,11 @@ $(function () {
     });
   });
 });
-
+*/
 /**-----------------------------------------
  * realTIme test
  * -----------------------------------------*/
+/*
 db.getHourUpSize()
 .done(function (data) {
   var n = 100, duration = 700, count = 0;
@@ -432,4 +539,4 @@ db.getHourUpSize()
       ++count;
     }
   }
-});
+});*/
