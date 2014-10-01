1ab04e75ef298aafd33b08ae47a9a3de6ba7c4bb
kohei-kp <cowhey1991@gmail.com>
2014-10-01 11:02:42 +0900

    Add bar and line chart.

--- cld3/cld3.js
+++ cld3/cld3.js
@@ -485,11 +485,10 @@ function createRealTimeChart() {
  * Bar Line Chart
  *---------------------------------------*/
 function createBarLine() {
-  var margin = { top: 10, right: 10, bottom: 100, left: 40 },
-      width = 800 - margin.left - margin.right,
-      height = 500 - margin.top - margin.bottom,
-
-      offsetX = 50, offsetY = 30, barMargin = 15,
+ var margin = { top: 30, right: 20, bottom: 30, left: 50 },
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom,
+ 
 
       svg, bar, line, 
 
@@ -499,80 +498,134 @@ function createBarLine() {
       // スケール
       xScale, yScale, y2Scale,
 
-      // label
-      labelList = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Set'];
+      parseDate = d3.time.format('%Y-%m-%d').parse;
 
   // svg生成
-  svg = util.createSVG('#bar-line', width + 10, height + 10);
+  svg = util.createSVG('#bar-line',
+    width + margin.left + margin.right,
+    height + margin.top + margin.bottom
+  );
+
+  svg.append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top +')');
 
   // データ取得
-  db.getWeekUp1().done(function (data) {
-    var dataset = data.week_up,
-        // Size
-        maxSize, xScale, yScaleSize, colorSize,
-        // Cnt
-        maxCnt, yScaleCnt;
+  db.getArea().done(function (data) {
+    var dataset = data.area, line;
 
-    maxSize = d3.max(dataset.map(F('size')));
-    
-    xScale = d3.scale.linear().domain([1, 7])
-      .range([offsetX, 80 - offsetX]);
+    dataset.forEach(function (d) {
+      d.date = parseDate(d.date);
+      d.count = parseInt(d.count);
+      d.up_size = parseInt(d.up_size) / 1000000;
+    });
+ 
+    xScale = d3.time.scale()
+      .range([0, width]);
 
-    // bar chart scale
-    yScaleSize = d3.scale.linear()
-      .domain([0, maxSize])
-      .range([height - offsetY, offsetY]);
+    yScale = d3.scale.linear()
+      .range([height, 5]);
 
-    // color scale
-    //colorSize = d3.scale.linear()
-    //  .domain([0, maxSize])
-    //  .range(['#EEEEFF', '#0000FF']);
+    y2Scale = d3.scale.linear()
+      .range([height, 5]);
 
-    // cnt
-    maxCnt = d3.max(dataset.map(F('cnt')));
+    xAxis = d3.svg.axis()
+      .scale(xScale)
+      .orient('bottom');
 
-    // line chart
-    yScaleCnt = d3.scale.linear()
-      .domain([0, maxCnt])
-      .range([height - offsetY, offsetY]);
+    yAxis = d3.svg.axis()
+      .scale(yScale)
+      .orient('right');
 
-    xAxis = d3.svg.axis().scale(xScale).orient('bottom');
-    yAxis = d3.svg.axis().scale(yScaleSize).orient('right');
-    y2Axis = d3.svg.axis().scale(yScaleCnt).orient('left');
+    y2Axis = d3.svg.axis()
+      .scale(y2Scale)
+      .orient('right');
+
+    line = d3.svg.line()
+      .x(function (d) { return xScale(d.date); })
+      .y(function (d) { return yScale(d.count); });
+
+    xScale.domain(d3.extent(dataset.map(F('date'))));
+    yScale.domain(d3.extent(dataset.map(F('count'))));
+    y2Scale.domain([0, d3.max(dataset.map(F('up_size')))]);
 
     svg.append('g')
-      .attr('class', 'x axis')
-      .attr('transform', 'translate(0,' + height + ')')
+      .attr({
+        class: 'x axis',
+        transform: 'translate(0,' + height + ')'
+      })
       .call(xAxis);
 
     svg.append('g')
-      .attr('transform', 'translate(' + 10 + ', 0)')
       .attr('class', 'y axis')
-      .call(yAxis);
+      .attr('transform', 'translate(' + width + ', 0)')
+      .call(yAxis)
+      .append('text')
+      .attr({
+        transform: 'translate(-20,' + 20 + ') rotate(-90)',
+        y: 6,
+        dy: '.71em'
+      })
+      .style('text-anchor', 'end')
+      .text('Upload Count');
 
     svg.append('g')
       .attr('class', 'y axis')
-      .attr('transform', 'translate(' + (width - 10) + ', 0)')
-      .call(y2Axis);
+      .call(y2Axis)
+      .append('text')
+      .attr({
+        transform: 'translate(50, 20) rotate(-90)',
+        y: 6,
+        dy: '.71em'
+      })
+      .style('text-anchor', 'end')
+      .text('Upload Size /MB');
 
-    var xPos = function (i) { return i * (50 + 15) + offsetX; };
 
-    // draw chart
-    bar = svg.selectAll('rect').data(dataset).enter()
+    // draw graph
+    svg.selectAll('.bar').data(dataset).enter()
       .append('rect')
       .attr({
-        x: function (d, i) { return xPos(d.week); },
-        y: function (d, i) { return height - offsetY; },
-        width: 50,
-        height: 0,
-        fill: '#88BBFF'
+        class: 'bar',
+        id: function (d) { return d.up_size },
+        x: function (d) { return xScale(d.date); },
+        width: 10,
+        y: function (d) { return y2Scale(d.up_size); },
+        height: function (d) { return height - y2Scale(d.up_size); },
+        fill: '#CCF',
+        'stroke': 'white',
+        'stroke-width' : 1
       })
-      .transition().duration(1000)
-      .attr({
-        y: function (d, i) { return yScaleSize(d.size); },
-        height: function (d, i) { return height - yScaleSize(d.size) - offsetY; }
-      });
+     .on('mouseover', function (d) {
+        d3.select(this).attr('fill', '#B0B0FF');
 
-    console.log(dataset);
-  });
+        var xPos = parseFloat(d3.select(this).attr('x')),
+            yPos = parseFloat(d3.select(this).attr('y'))
+
+        svg.append('text')
+          .attr('fill', 'black')
+          .transition()
+          .duration(300)
+          .attr({
+            id: 'tooltip',
+            x: xPos,
+            y: yPos + 20,
+            'text-anchor': 'middle',
+            'font-family': 'sans-serif',
+            'font-size': '11px',
+            'font-weight': 'bold'
+          })
+          .text(d3.select(this).attr('id') + ' MB');
+    })
+    .on('mouseout', function () {
+      d3.select(this).attr('fill', '#CCF');
+      d3.select('#tooltip').remove();
+    });
+
+    svg.append('path')
+      .datum(dataset)
+      .attr({
+        class: 'line',
+        d: line
+      });
+   });
 }
